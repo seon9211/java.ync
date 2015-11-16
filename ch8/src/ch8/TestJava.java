b729fcb1e618ad4987661bb8b10bdf733bb11ae2
Y <Y@Y-PC>
2015-11-16 09:37:11 +0900

    2015.11.16

---
+++ ch8/src/ch8/TestJava.java
@@ -0,0 +1,13 @@
+package ch8;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1505082-±Çº´¼±");
+		
+		System.out.println("Hello world");
+
+	}
+
+}
