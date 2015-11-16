0864b738091a42663e088b7dcf21f8a874980702
Y <Y@Y-PC>
2015-11-16 11:58:41 +0900

    15.11.16 -3

---
+++ ch8/src/ch8/Box3Test1.java
@@ -0,0 +1,41 @@
+package ch8;
+
+class Box3 {
+	int width;
+	int height;
+	int depth;
+	long idNum;
+	static long boxID = 0;
+
+	public Box3() {
+		idNum = ++boxID;
+	}
+	public void sum(){
+		System.out.println("sum");
+	}
+}
+
+public class Box3Test1 {
+	
+	public void sum(){
+		System.out.println("sum2");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1505082-권병선");
+
+		Box3 mybox1 = new Box3();
+		Box3 mybox2 = new Box3();
+		Box3 mybox3 = new Box3();
+		Box3 mybox4 = new Box3();
+
+		System.out.println("mybox1의 id 번호 : " + mybox1.idNum);
+		System.out.println("mybox2의 id 번호 : " + mybox2.idNum);
+		System.out.println("mybox3의 id 번호 : " + mybox3.idNum);
+		System.out.println("mybox4의 id 번호 : " + mybox4.idNum);
+		System.out.println("마지막 생성된 박스 번호는 " + Box3.boxID + "번 입니다.");
+
+	}
+
+}
